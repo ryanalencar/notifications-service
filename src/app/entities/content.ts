80fde641b786a6e201faf7ebd52034e46a871b4a
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-12-15 09:34:46 -0300

    ✨ feat: notification content value object

---
+++ src/app/entities/content.ts
@@ -0,0 +1,19 @@
+export class Content {
+  private readonly content: string;
+
+  constructor(content: string) {
+    const isContentLengthValid = this.validateContentLength(content);
+
+    if (!isContentLengthValid) throw new Error('Content length error');
+
+    this.content = content;
+  }
+
+  private validateContentLength(content: string): boolean {
+    return content.length >= 5 && content.length <= 240;
+  }
+
+  get value(): string {
+    return this.content;
+  }
+}
