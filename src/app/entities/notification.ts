f0e73d7ad4b2350171c4f931c76a246730e527f8
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-12-15 09:34:17 -0300

    ✨ feat: notification getters and setters

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,6 +1,8 @@
+import { Content } from './content';
+
 interface INotificationProps {
   recipientId: string;
-  content: string;
+  content: Content;
   category: string;
   readAt?: Date | null;
   createdAt: Date;
@@ -12,11 +14,39 @@ export class NotificationsService {
     this.props = props;
   }
 
-  public set content(content: string) {
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId() {
+    return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
     this.props.content = content;
   }
 
   public get content() {
     return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category() {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
   }
 }
