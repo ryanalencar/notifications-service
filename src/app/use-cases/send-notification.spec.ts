01c6e63e31930cd1fac5c33b315dd3328de4250a
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-12-26 13:25:03 -0300

    ✨ feat(notification): notification repository contract

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,9 +1,16 @@
 import { randomUUID } from 'crypto';
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    console.log(notification);
+  },
+};
 
 describe('Send notifications', () => {
   it('should be able to send notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
       category: 'social',
