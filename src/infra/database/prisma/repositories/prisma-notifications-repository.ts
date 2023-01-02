7992e567a81d70bc9004e9b50e139bd9a7d832ba
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-02 13:17:53 -0300

    ✨ feat(notification): adding notifications repository to database-module providers

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { Notification } from 'src/app/entities/notification';
 import { NotificationsRepository } from 'src/app/repositories/notifications-repository';
 import { PrismaService } from '../prisma.service';
 
+@Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) { }
 
