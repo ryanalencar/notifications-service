2612e4ace7e64b803f0fe4648d4d48a3f32ffa13
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-02 11:54:20 -0300

    ✨ feat(notification): private _id property

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto';
 import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
@@ -10,13 +11,19 @@ interface INotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: INotificationProps;
 
   constructor(props: Replace<INotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
     };
+  }
+
+  public get id(): string {
+    return this._id;
   }
 
   public set recipientId(recipientId: string) {
