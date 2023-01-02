026b4f5c64661f2697b7b90f59bdc805440c0fa9
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-02 11:56:44 -0300

    🦄 refactor(app): new app module and folders structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
+import { DatabaseModule } from './infra/database/database.module';
+import { HTTPModule } from './infra/http/http.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [HTTPModule, DatabaseModule],
 })
 export class AppModule { }
