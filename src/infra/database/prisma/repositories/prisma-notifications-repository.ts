f8eb44d19626f1bc8a6f3b8e79f720af10cd339e
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-03 12:05:24 -0300

    ✨ feat(notification): new absctract methods to notifications repository

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -8,10 +8,30 @@ import { PrismaService } from '../prisma.service';
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) { }
 
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    const recipientNotificationsCount =
+      await this.prismaService.notification.count({
+        where: { recipientId },
+      });
+    return recipientNotificationsCount;
+  }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    // const notification = await this.prismaService.notification.findUnique({
+    //   where: { id: notificationId },
+    // });
+    // return notification || null;
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification): Promise<void> {
     const notificationRaw = PrismaNotificationMapper.toPrisma(notification);
     await this.prismaService.notification.create({
       data: notificationRaw,
     });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -5,7 +5,31 @@ export class InMemoryNotificationsRepository
   implements NotificationsRepository {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (notification) => notification.id === notificationId,
+    );
+    return notification || null;
+  }
+
   async create(notification: Notification): Promise<void> {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (_notification) => _notification.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    const count = this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    ).length;
+    return count;
   }
 }
