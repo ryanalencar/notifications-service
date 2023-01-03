90f852ae30806dfc7cd5e23bbddb047517f60a0f
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-03 19:14:09 -0300

    ✨ feat(prisma-mappers): toDomain mapper function

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Content } from '@app/entities/content';
 import { Notification } from '@app/entities/notification';
+import { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       recipientId: notification.recipientId,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+        readAt: raw.readAt,
+      },
+      raw.id,
+    );
   }
 }
