2b21a966bcd906659cab8c4a30d78a0dc78b804b
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-04 11:35:57 -0300

    ✨ feat(http): notifications controllers routes

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { SendNotification } from 'src/app/use-cases/send-notification';
+
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@app/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@app/use-cases/get-recipient-notifications';
+import { SendNotification } from '@app/use-cases/send-notification';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
+
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HTTPModule { }
